59b92d72dd3c30d5977213faad4e90314e52dc68
Amir Kabbouri <amir@mac.home>
2026-01-30 16:39:57 +0100

    fix s3

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,6 +1,5 @@
 try:
-    import os, hashlib
-    import traceback
+    import os, traceback
     import pgcontents
     from jupyter_server.auth import passwd
 
@@ -8,7 +7,6 @@ try:
 
     c.ServerApp.root_dir = '/'
     uid = os.environ.get('SCALINGO_UID', '')
-
 
     passwd_env = os.environ.get("JUPYTER_NOTEBOOK_PASSWORD", "")
     if passwd_env:
@@ -19,13 +17,29 @@ try:
 
     c.ServerApp.terminado_settings = {'shell_command': ['/bin/bash']}
 
-    db_url = os.getenv('DATABASE_URL', None) or os.getenv('SCALINGO_POSTGRESQL_URL', '')
-    if db_url and db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
-    if db_url:
+    s3_access_key = os.getenv("S3_ACCESS_KEY")
+    s3_secret_key = os.getenv("S3_SECRET_KEY")
+    s3_bucket_name = os.getenv("S3_BUCKET_NAME")
+    s3_endpoint = os.getenv("S3_ENDPOINT")
+
+    if s3_access_key and s3_secret_key and s3_bucket_name:
+
+        from s3contents import S3ContentsManager
+        c.ServerApp.contents_manager_class = S3ContentsManager
+        c.S3ContentsManager.bucket = s3_bucket_name
+        c.S3ContentsManager.prefix = ""  
+        c.S3ContentsManager.key_id = s3_access_key
+        c.S3ContentsManager.secret_key = s3_secret_key
+        c.S3ContentsManager.endpoint_url = s3_endpoint
+        print("Using S3ContentsManager")
+    else:
+        db_url = os.getenv('DATABASE_URL') or os.getenv('SCALINGO_POSTGRESQL_URL', '')
+        if db_url.startswith("postgres://"):
+            db_url = db_url.replace("postgres://", "postgresql://", 1)
         c.ServerApp.contents_manager_class = pgcontents.PostgresContentsManager
         c.PostgresContentsManager.db_url = db_url
         c.PostgresContentsManager.user_id = uid
+        print("Using pgcontents/PostgresContentsManager")
 
 except Exception:
     traceback.print_exc()
